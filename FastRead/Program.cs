13a5a3d4e7f9b5c179a5a96438839a80396c08d7
Dmitry-Dzyuba <diman.secret@mail.ru>
2022-09-18 11:27:01 +0300

    CompletedTask

--- FastRead/Program.cs
+++ FastRead/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Concurrent;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -9,9 +10,14 @@ namespace FastRead
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            ParallelCheck();
+        }
+        private static void ParallelCheck()
+        {
             var path = @"../../";
             var separators = new[] { ',', '.', ' ', ';', ':', '?', '!', '(', ')', '*', '"', '[', ']' };
             var result = new ConcurrentDictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
+
             File.ReadLines(path + @"\test.txt")
                 .AsParallel()
                 .ForAll(line =>
@@ -29,7 +35,7 @@ namespace FastRead
             var res = result
                 .OrderByDescending(u => u.Value)
                 .ToDictionary(d => d.Key, d => d.Value);
-            var resultFile = path + @"\result.txt";
+            var resultFile = path + @"\parallel_result.txt";
             if (File.Exists(resultFile))
                 File.Delete(resultFile);
             else
@@ -40,6 +46,38 @@ namespace FastRead
                 foreach (var pair in res) sw.WriteLine(pair);
             }
         }
+        private static void SequentialCheck()
+        {
+            var path = @"../../";
+            var separators = new[] { ',', '.', ' ', ';', ':', '?', '!', '(', ')', '*', '"', '[', ']' };
+            var sq = new Dictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
+            using (StreamReader sr = new(path + @"\test.txt"))
+            {
+                string? line = "";
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.ToLower();
+                    line = Regex.Replace(line, @"\[\d+\]", " "); // footnotes
+                    line = Regex.Replace(line, @"^m{0,4}(cm|cd|d?c{0,3})?(xc|xl|l?x{0,3})?(ix|iv|v?i{0,3})?\.$", " "); // romain letters
+                    line = Regex.Replace(line, @"\-\s", " "); // double dashes
+                    line = Regex.Replace(line, @"[0-9]+", " ");
+                    foreach (var word in line.Split(separators, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        if (!sq.ContainsKey(word))
+                        {
+                            sq.Add(word, 1);
+                        }
+                        else
+                        {
+                            sq[word]++;
+                        }
+                    }
+                }
+            }
+            using (StreamWriter sw = new StreamWriter(path + @"\sequential_result.txt"))
+            {
+                foreach (var pair in sq.OrderByDescending(u => u.Value)) sw.WriteLine(pair);
+            }
+        }
     }
-
 }
