e04e6760f78633abbc9c640f072ac0c776639070
Dmitry-Dzyuba <diman.secret@mail.ru>
2022-10-17 10:52:29 +0300

    Realizing a server-client connection

--- WordFrequency/Frequency.cs
+++ WordFrequency/Frequency.cs
@@ -1,60 +1,44 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
-namespace FastRead
+namespace WordFrequency
 {
-    internal class Program
+    public static class Frequency
     {
-        //static void Main(string[] args)
-        //{
-        //    Console.OutputEncoding = System.Text.Encoding.UTF8;
-        //    ParallelCheck();
-        //}
-        private static Dictionary<string, uint> ParallelCheck(string path)
+        public static Dictionary<string, uint> ParallelCheck(string[] elements)
         {
-            //var path = @"../../../";
             var separators = new[] { ',', '.', ' ', ';', ':', '?', '!', '(', ')', '*', '"', '[', ']' };
             var result = new ConcurrentDictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
-
-            File.ReadLines(path)
-                .AsParallel()
-                .ForAll(line =>
+            Parallel.ForEach(elements, line =>
+            {
+                line = line.ToLower();
+                line = Regex.Replace(line, @"\[\d+\]", " "); // footnotes
+                line = Regex.Replace(line, @"^m{0,4}(cm|cd|d?c{0,3})?(xc|xl|l?x{0,3})?(ix|iv|v?i{0,3})?\.$",
+                    " "); // romain letters
+                line = Regex.Replace(line, @"\-\s", " "); // double dashes
+                line = Regex.Replace(line, @"[0-9]+", " ");
+                foreach (var word in line.Split(separators, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    line = line.ToLower();
-                    line = Regex.Replace(line, @"\[\d+\]", " "); // footnotes
-                    line = Regex.Replace(line, @"^m{0,4}(cm|cd|d?c{0,3})?(xc|xl|l?x{0,3})?(ix|iv|v?i{0,3})?\.$", " "); // romain letters
-                    line = Regex.Replace(line, @"\-\s", " "); // double dashes
-                    line = Regex.Replace(line, @"[0-9]+", " ");
-                    foreach (var word in line.Split(separators, StringSplitOptions.RemoveEmptyEntries))
-                    {
-                        result.AddOrUpdate(word, 1, ((s, u) => u + 1));
-                    }
-                });
+                    result.AddOrUpdate(word, 1, ((s, u) => u + 1));
+                }
+            });
             var res = result
                 .OrderByDescending(u => u.Value)
                 .ToDictionary(d => d.Key, d => d.Value);
-            //var resultFile = path + @"\parallel_result.txt";
-            //if (File.Exists(resultFile))
-            //    File.Delete(resultFile);
-            //else
-            //    File.Create(resultFile).Close();
-
-            //using (StreamWriter sw = new StreamWriter(resultFile))
-            //{
-            //    foreach (var pair in res) sw.WriteLine(pair);
-            //}
             return res;
         }
         private static Dictionary<string, uint> SequentialCheck(string path)
         {
-           // var path = @"../../../";
             var separators = new[] { ',', '.', ' ', ';', ':', '?', '!', '(', ')', '*', '"', '[', ']' };
             var sq = new Dictionary<string, uint>(StringComparer.InvariantCultureIgnoreCase);
             using (StreamReader sr = new(path))
             {
-                string? line = "";
+                var line = "";
                 while ((line = sr.ReadLine()) != null)
                 {
                     line = line.ToLower();
@@ -63,12 +47,10 @@ namespace FastRead
                     line = Regex.Replace(line, @"\-\s", " "); // double dashes
                     line = Regex.Replace(line, @"[0-9]+", " ");
                     foreach (var word in line.Split(separators, StringSplitOptions.RemoveEmptyEntries))
-                    {
                         if (!sq.ContainsKey(word))
                             sq.Add(word, 1);
                         else
                             sq[word]++;
-                    }
                 }
             }
             var res = sq
